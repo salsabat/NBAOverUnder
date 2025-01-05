f8257e2228c43804365401cd80230342cb3b94e2
Sajal Sabat <sajalsabat@gmail.com>
2025-01-04 21:28:30 -0500

    GUI
    Set up all the components of the interface without functionality.

---
+++ GUI/MoneyLinePredictor/src/PredictorApp.java
@@ -0,0 +1,99 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+/**
+ * A graphical application to run the prediction model.
+ */
+public class PredictorApp {
+
+    /**
+     * Our application window. Disposed when application exits.
+     */
+    private JFrame frame;
+
+    /**
+     * Input and output components.
+     */
+    private JTextField playerName;
+    private JTextField stat;
+    private JTextField moneyLine;
+    private JButton predict;
+    private JLabel predictionResult;
+
+    /**
+     * Construct a new application instance. Initializes GUI components, so must be invoked on the
+     * Swing Event Dispatch Thread. Does not show the application window (call `start()` to do
+     * that).
+     */
+    public PredictorApp() {
+        frame = new JFrame("NBA Money Line Predictor");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setPreferredSize(new Dimension(450, 450));
+
+        JPanel tempPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        JLabel playerNameQ = new JLabel("Enter Player Name:");
+        playerName = new JTextField(15);
+        tempPanel.add(playerNameQ);
+        tempPanel.add(playerName);
+        panel.add(tempPanel);
+
+        tempPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        JLabel statQ = new JLabel("Enter Stat:");
+        stat = new JTextField(10);
+        tempPanel.add(statQ);
+        tempPanel.add(stat);
+        panel.add(tempPanel);
+
+        tempPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        JLabel moneyLineQ = new JLabel("Enter Money Line:");
+        moneyLine = new JTextField(5);
+        tempPanel.add(moneyLineQ);
+        tempPanel.add(moneyLine);
+        panel.add(tempPanel);
+
+        tempPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        predict = new JButton("Predict");
+        predict.addActionListener(e -> {});
+        tempPanel.add(predict);
+        panel.add(tempPanel);
+
+        tempPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        predictionResult = new JLabel("Result: N/A");
+        tempPanel.add(predictionResult);
+        panel.add(tempPanel);
+
+        panel.add(Box.createVerticalGlue());
+
+        frame.add(panel);
+    }
+
+    /**
+     * Start the application by showing its window.
+     */
+    public void start() {
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Run an instance of PredictorApp. No program arguments are expected.
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Set Swing theme to look the same (and less old) on all operating systems.
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception ignored) {
+                /* If the Nimbus theme isn't available, just use the platform default. */
+            }
+
+            // Create and start the app
+            PredictorApp app = new PredictorApp();
+            app.start();
+        });
+    }
+}
